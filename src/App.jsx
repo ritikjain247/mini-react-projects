14a56894a2978717657b80becb3d3f48f7683a3a
ritikjain247 <ritikjain247@gmail.com>
2024-07-31 19:41:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ProgressBars from './components/ProgressBar'
+import TempConverter from './components/TempConverter'
+
+function App() {
+
+  return (
+    <>
+      <TempConverter />
+      <ProgressBars/>
+    </>
+  )
+}
+
+export default App
